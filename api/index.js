5aa6fe0743a418270b3236e496adbf837edc762b
Kaden Wingert <a17125@MacBook-Air.local>
2023-12-15 16:19:17 -0600

    created the add employee screen

--- api/index.js
+++ api/index.js
@@ -1,24 +1,79 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 8000;
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb+srv://kaden:kaden@cluster0.dujw8qk.mongodb.net/", {
+mongoose
+  .connect("mongodb+srv://kaden:kaden@cluster0.dujw8qk.mongodb.net/", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    console.log("Connected to MongoDB")
-}).catch((error) => {
-    console.log("Error connecting to MongoDB", error)
-});
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB", error);
+  });
 
 app.listen(port, () => {
-    console.log("Server is running on port 8000");
-})
+  console.log("Server is running on port 8000");
+});
+
+const Employee = require("./models/employee");
+const Addendance = require("./models/attendance");
+//endpoint to register an employee
+app.post("/addEmployee", async (req, res) => {
+  try {
+    const {
+      employeeName,
+      employeeID,
+      desription,
+      phoneNumber,
+      dateOfBirth,
+      joiningDate,
+      ativeEmployee,
+      salary,
+      address,
+    } = req.body;
+
+    //create a new employee
+    const newEmployee = new Employee({
+      employeeName,
+      employeeID,
+      desription,
+      phoneNumber,
+      dateOfBirth,
+      joiningDate,
+      ativeEmployee,
+      salary,
+      address,
+    });
+
+    //save the employee
+    await newEmployee.save();
+    res.status(201).json({
+      message: "Employee registered successfully",
+      employee: newEmployee,
+    });
+  } catch (error) {
+    console.log(error);
+    res.setStatus(500).json({ message: "Failed to register an employee" });
+  }
+});
+
+//endpoint to fetch all the employees
+app.get("/employees", async (req, res) => {
+  try {
+    const employees = await Employee.find();
+    res.status(200).json(employees);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to retrieve the employees" });
+  }
+});
--- api/models/attendance.js
+++ api/models/attendance.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const attendanceSchema = new mongoose.Schema({
-    employeeID: {
-        type: String,
-        required: true,
-    },
-    employeeName: {
-        type: String,
-        required: true,
-    },
-    date: {
-        type: String,
-        required: true,
-    },
-    status: {
-        type: String,
-        required: true,
-    },
+  employeeId: {
+    type: String,
+    required: true,
+  },
+  employeeName: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
 });
 
 const Attendance = mongoose.model("Attendance", attendanceSchema);
